23752acfb801f8102d95a36a1229586e3d969c55
patricia-faustino <patriciafaustinobr@gmail.com>
2021-09-30 21:15:58 -0300

    retirando responsabilidade de criar categorias da rota

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,19 @@
-import { response, Router } from "express";
+import { response, Router } from 'express';
 
-import { Category } from "../model/Category";
+import { Category } from '../model/Category';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body;
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+  
+  categoriesRepository.create({ name, description });
 
-    const category =  new Category();
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    });
-    
-    categories.push(category);
-
-    return response.status(201).json({ category });
+  return response.status(201).send();
 });
 
-export { categoriesRoutes }
+export { categoriesRoutes };
