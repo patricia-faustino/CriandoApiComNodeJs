510868a95e0426fee599b701c654e3f1a0bdeadc
patricia-faustino <patriciafaustinobr@gmail.com>
2021-10-03 22:57:59 -0300

    refatorando specifications e retirando imports nao utilizados

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -3,11 +3,14 @@ import {
   ICreateSpecificationDTO,
   ISpecificationRepository,
 } from '../ISpecificationRepository';
+import { CategoriesRepository } from './CategoriesRepository';
 
 class SpecificationsRepository implements ISpecificationRepository {
   private specifications: Specification[];
 
-  constructor() {
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
     this.specifications = [];
   }
 
@@ -28,6 +31,13 @@ class SpecificationsRepository implements ISpecificationRepository {
     );
 
     return specification;
+  }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
   }
 }
 
