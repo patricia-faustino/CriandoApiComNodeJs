4cdf2f37acb9546a4db50f8bb601033a118a5a7c
patricia-faustino <patriciafaustinobr@gmail.com>
2021-10-04 22:31:53 -0300

    refatorando importacao com use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const upload = multer({ dest: './tmp' });
@@ -18,11 +19,7 @@ categoriesRoutes.get('/', (request, response) => {
 });
 
 categoriesRoutes.post('/import',  upload.single("file"), (request, response) => {
-  const { file } = request;
-
-  console.log(file);
-  
-  return response.send();
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
