a1c7c943c3dde0179682656aed60357b546c1e0f
patricia-faustino <patriciafaustinobr@gmail.com>
2021-11-11 00:07:26 -0300

    adicionando funcao para remover avatar caso usuario já possua avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { IUserRepository } from "../../repositories/IUsersRepository";
 
+import { deleteFile } from "../../../../utils/file"
 interface IRequest {
     user_id: string;
     avatar_file: string;
@@ -20,6 +21,9 @@ class UpdateUserAvatarUseCase{
 
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+        
+        if(user.avatar)
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
 
         user.avatar = avatar_file;
 
